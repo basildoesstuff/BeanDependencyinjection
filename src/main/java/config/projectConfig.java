a3d91d4e5d857a38b3ee59e903bad5672c120c09
basildoesstuff <contact.basilsunny@gmail.com>
2023-02-23 23:08:31 +0530

    Correction:@ComponentScan is only used for stereotype annotation

--- src/main/java/config/projectConfig.java
+++ src/main/java/config/projectConfig.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan(basePackages = "MyBean")
 public class projectConfig {
     @Bean(name = "Tommy")
     Pet pet1(){
